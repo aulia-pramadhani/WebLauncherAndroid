8f50226993cb4b50c07a6ccf21ba5e91083667d1
Aulia Pramadhani <dhani9aul@gmail.com>
2015-04-09 16:24:21 +0700

    Add Project
    Publish to repo

---
+++ src/com/example/weblauncher/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.weblauncher;
+
+import com.example.weblauncher.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+//import android.view.Menu;
+//import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	private WebView mWebView;
+	
+	@Override 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+ 
+		mWebView = (WebView) findViewById(R.id.activity_main_webview);
+		mWebView.setWebViewClient(new WebViewClient());
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+		mWebView.loadUrl("http://www.gamefaqs.com/");
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if(event.getAction() == KeyEvent.ACTION_DOWN){
+	        switch(keyCode)
+	        {
+	        case KeyEvent.KEYCODE_BACK:
+	            if(mWebView.canGoBack()){
+	                mWebView.goBack();
+	            }else{
+	                finish();
+	            }
+	            return true;
+	        }
+
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle action bar item clicks here. The action bar will
+//		// automatically handle clicks on the Home/Up button, so long
+//		// as you specify a parent activity in AndroidManifest.xml.
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
+}

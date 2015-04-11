30db627325a0bc6a21dc917d701c269e668bce04
Aulia Pramadhani <dhani9aul@gmail.com>
2015-04-11 19:32:23 +0700

    add settingsActivity

--- src/com/example/weblauncher/MainActivity.java
+++ src/com/example/weblauncher/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.weblauncher;
 import com.example.weblauncher.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.view.KeyEvent;
@@ -16,16 +19,12 @@ public class MainActivity extends Activity {
 	
 	@Override 
 	public void onCreate(Bundle savedInstanceState) {
-		url = "www.gamefaqs.com";
+		
+		loadSetting();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
  
-		mWebView = (WebView) findViewById(R.id.activity_main_webview);
-		mWebView.setWebViewClient(new MyWebViewClient(url));
-		WebSettings webSettings = mWebView.getSettings();
-		webSettings.setJavaScriptEnabled(true);
-		
-		mWebView.loadUrl("http://"+url);
+		loadWebView(this.url);
 	}
 	
 	@Override
@@ -58,10 +57,40 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		  
+		Intent intent = new Intent();
+		intent.setClass(MainActivity.this, SettingsActivity.class);
+		startActivityForResult(intent, 0); 
+		
+		return true;
 	}
+	
+	 @Override
+	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		//super.onActivityResult(requestCode, resultCode, data);
+		  
+		/*
+		 * To make it simple, always re-load Preference setting.
+		 */
+		
+		loadSetting();
+		loadWebView(this.url);
+	 }
+	 
+	 private void loadSetting(){
+		  SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+		  this.url = mySharedPreferences.getString("webUrl", "google.co.id");
+
+	}
+	 
+	 private void loadWebView(String url){
+		mWebView = (WebView) findViewById(R.id.activity_main_webview);
+		mWebView.setWebViewClient(new MyWebViewClient(url));
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+		mWebView.loadUrl("http://"+url);
+	 }
 }
